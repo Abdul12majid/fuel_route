5062e09a56ca93f3d8b7a90328b8573db73af1d7
Abdul12majid <yisaabdulmajid@gmail.com>
2025-01-13 13:45:13 +0100

    app set-up

--- fuel_route/settings.py
+++ fuel_route/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'route_app'
 ]
 
 MIDDLEWARE = [
---
+++ route_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def index(request):
+	return Response({"hello"})
