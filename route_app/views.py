7918223d838b82a97c8a587ef20c057d97926e3b
Abdul12majid <yisaabdulmajid@gmail.com>
2025-01-14 13:45:22 +0100

    calc total amount func => views.py

--- route_app/views.py
+++ route_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RouteRequestSerializer
-from .utils import get_route_details, find_optimal_locations
+from .utils import get_route_details, find_optimal_locations, calculate_total_amount
 
 @api_view(['GET'])
 def index(request):
@@ -24,13 +24,17 @@ def get_route(request):
         if route_details:
             # Find the optimal locations based on the distances and addresses
             optimal_locations = find_optimal_locations(route_details['distances_miles'], route_details['addresses'])
+
+            # Calculate the total fuel cost
+            total_amount = calculate_total_amount(optimal_locations)
             
             # Prepare the response data
             response_data = {
                 "total_distance_miles": route_details["total_distance_miles"],
                 "stop addresses": route_details["addresses"],
                 "stop addresses distances_miles": route_details["distances_miles"],
-                "optimal_locations (fuel stops)": optimal_locations
+                "optimal_locations (fuel stops)": optimal_locations,
+                "total_fuel_cost": total_amount
             }
             
             return Response(response_data, status=status.HTTP_200_OK)
